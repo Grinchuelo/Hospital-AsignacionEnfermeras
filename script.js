923a9b80da61ed8e5c88d18029acd1025e447d42
Grin <santinogreen14@gmail.com>
2024-06-09 20:42:27 -0300

    agregué todo el plano entero. Asigné vectores para los sectores disabled y los otros. Todavia no terminé de asignarle los colores a cada uno

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sectores = document.querySelectorAll('#op_industriales, #direccion');
+const sectoresDisabled = document.querySelectorAll('#op_industriales, #direccion, #administracion, #laboratorio, #esterilizacion, #comedor, #bodega, #vestuario, #central_alimentacion');
+const sectores = document.querySelectorAll('#auditorio, #rehabilitacion, #pnac, #farmacia, #sectorAzul, #sectorVerde, #sectorAmarillo, #hospitalizacion, #urgencia');
+const auditorio = document.getElementById('auditorio');
+const rehabilitacion = document.getElementById('rehabilitacion');
+const pnac = document.getElementById('pnac');
+const farmacia = document.getElementById('farmacia');
+const sectorAzul = document.getElementById('sectorAzul');
+const sectorVerde = document.getElementById('sectorVerde');
+const sectorAmarillo = document.getElementById('sectorAmarillo');
+const hospitalizacion = document.getElementById('hospitalizacion');
+const urgencia = document.getElementById('urgencia');
 
-    sectores.forEach((sector) => {
+document.addEventListener('DOMContentLoaded', () => {
+    sectoresDisabled.forEach((sector) => {
         sector.classList.add('transition');
         sector.style.fill = "#818284";
     })
 
     sectores.forEach((sector) => {
+        sector.classList.add('transition');
+    })
+
+    auditorio.style.fill = "#20487b";
+    rehabilitacion.style.fill = "#feca06"; rehabilitacion.setAttribute("fill", "#000")
+
+    sectoresDisabled.forEach((sector) => {
         if (sector) {
             sector.addEventListener('mouseover', () => {
                 sector.classList.add('cambiarColor');
@@ -15,5 +32,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 sector.classList.remove('cambiarColor');
             })
         }     
+    })
+
+    const square = document.getElementById('tspan3-0');
+    square.addEventListener('mouseover', () => {
+        square.textContent = square.textContent + " 1";
     })
 })
